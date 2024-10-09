fd0f5e2a8cc3c3c8714437e63b12ae3a0a212a44
Prateek Sagar <prateek.intern@faircent.com>
2024-10-09 14:17:30 +0530

    css changes

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -9,7 +9,7 @@ const Skills = () => {
       </h2>
       <div className="grid grid-cols-3  sm:grid-cols-5 gap-4 mt-5 dark:text-black">
           {techStack.map((tech) => (
-            <div key={tech.name} className="flex flex-col items-center dark:bg-slate-300  border rounded-md">
+            <div key={tech.name} className="flex flex-col items-center dark:bg-opacity-60 hover:shadow-md shadow-slate-400 dark:bg-gray-900 dark:text-white  border rounded-md">
               <div className="w-8 h-8  md:w-12 md:h-12 mb-2">
                 <img src={tech.icon} alt={tech.name} className="w-full h-full object-contain dark:text-white" />
               </div>
