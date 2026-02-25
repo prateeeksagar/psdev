bdc90f6c8f5e586c1dfbafe4dc3d66ed4bddc742
prateek sagar <prateeksagar2103@gmail.com>
2026-02-25 23:31:45 +0530

    resolved: resolve lint errors

--- src/components/portfolio/experience/Experience.tsx
+++ src/components/portfolio/experience/Experience.tsx
@@ -6,7 +6,7 @@ const Experience = () => {
     <div className="">
       <h2 className="text-2xl font-semibold p-2 border-b">Experience</h2>
       <div className="">
-        {WorkExperience.map(exp => (<div className=""><ExperienceCard data={exp}/></div>))}
+        {WorkExperience.map((exp,idx) => (<div key={idx} className=""><ExperienceCard data={exp}/></div>))}
         </div>
     </div>
   )
