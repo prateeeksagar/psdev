f6508823cc41a49610a42bed06e8d133460b8591
prateek sagar <prateeksagar2103@gmail.com>
2026-03-06 10:28:58 +0530

    add: experience UI fix

--- src/app/features/portfolio/experience/Experience.tsx
+++ src/app/features/portfolio/experience/Experience.tsx
@@ -6,9 +6,9 @@ import { Badge } from "@/components/ui/badge";
 const Experience = () => {
   return (
     <div className="">
-      <div className="flex flex-row justify-between items-center p-2">
-      <h2 className="text-2xl font-semibold border-b">Experience</h2>
-      <Badge variant={"secondary"} className="w-fit h-fit"><span>3+ years</span></Badge>
+      <div className="flex flex-row justify-between items-center p-2 border-b">
+      <h2 className="text-2xl font-semibold">Experience</h2>
+      <Badge variant={"secondary"} className="w-fit h-fit"><span>Total: 3+ years</span></Badge>
       </div>
       <div className="">
         {WorkExperience.map((exp,idx) => (<div key={idx} className=""><ExperienceCard data={exp}/></div>))}
